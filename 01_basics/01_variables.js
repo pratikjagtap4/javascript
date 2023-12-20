9d5b7577c1f5270102da377b74594e9c9509cb50
Pratik Jagtap <97509037+pratikjagtap4@users.noreply.github.com>
2023-12-20 05:43:34 +0000

    type of variables

--- 01_basics/01_variables.js
+++ 01_basics/01_variables.js
@@ -5,7 +5,14 @@ accountCity= "pune"
 let accountState 
 
 // console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
+// accountId = 456  is not possible as const cant be update
 // console.log(accountId)
 
-var accountPassword = 123456789
+
+// let accountEmail = "jagtap" variables withnlet cannot be redeclared in same scope unllike var 
+accountEmail="jagtap" // but we can update the variables declared using let
+console.log(accountEmail)
+
+
+var accountPassword = 123456789 // we can redeclared as well as update variables declared using var 
 console.log(accountPassword);
