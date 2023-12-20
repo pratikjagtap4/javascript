e6d567d51018be311bf9617be44a9debb78a0089
Pratik Jagtap <97509037+pratikjagtap4@users.noreply.github.com>
2023-12-20 05:43:53 +0000

    data types

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,11 @@
+let name = " pratik"
+let age = 23
+let isLoggedIn =true
+let state = null
+let job
+
+// console.log(typeof name);
+// console.log(typeof age);
+// console.log(typeof isLoggedIn);
+console.log(typeof state); // type of null is object 
+console.log(typeof job)
