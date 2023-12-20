7d0b401a62596ce868142b85b48e32fefc6bbced
Pratik Jagtap <97509037+pratikjagtap4@users.noreply.github.com>
2023-12-20 05:01:29 +0000

    variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,11 @@
+const accountId = 12345
+let accountEmail = "pratikjagtap"
+var accountPassword = 987654
+accountCity= "pune"
+let accountState 
+
+// console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
+// console.log(accountId)
+
+var accountPassword = 123456789
+console.log(accountPassword);
