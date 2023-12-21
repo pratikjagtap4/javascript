34350c0f85c930d29ca4606b3b43facdfeac8192
Pratik Jagtap <97509037+pratikjagtap4@users.noreply.github.com>
2023-12-21 12:28:38 +0000

    comparison operators

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,14 @@
+// console.table([2>1, 2>=1, 2<1, 2<=1 , 2>2 , 2<2 , 2==1 , 2!=1])
+
+// console.log(5>"7")
+// console.log("4b">6)
+
+// console.log(null>0)
+// console.log(null==0)
+// console.log(null>=0)
+// console.log(1>null)
+
+// console.log(undefined>0)
+// console.log(undefined==0)
+// console.log(undefined>=0)
+
