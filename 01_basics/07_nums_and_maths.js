b399ba8abfd884d3cd1acc296dd123cb56a1903b
Pratik Jagtap <97509037+pratikjagtap4@users.noreply.github.com>
2023-12-22 13:13:10 +0000

    math and num

---
+++ 01_basics/07_nums_and_maths.js
@@ -0,0 +1,25 @@
+let number = new Number(100.982)
+// console.log(number);
+
+// console.log(number.toString().length)
+
+// console.log(number.toFixed(3));  // gives precision 
+
+// console.log(number.toPrecision(4))
+
+let hundreds = 1000000
+// console.log(hundreds.toLocaleString('en-IN'));
+
+//  *************** Maths *****************
+
+// console.log(Math.abs(-4))
+// console.log(Math.round(4.7))
+// console.log((Math.ceil(4.2)));
+// console.log(Math.floor(4.9))
+// console.log((Math.min(1,2,7,3,5)));
+// console.log((Math.max(12,3,4,5,6)));
+
+const min =10
+const max = 20
+
+console.log(Math.floor(Math.random() *(max-min+1)+min))
