4d34cfa9e74687f919d37ddbd4339a3a7c9f6d28
Pratik Jagtap <97509037+pratikjagtap4@users.noreply.github.com>
2023-12-27 12:16:34 +0000

    IIFE

---
+++ 03_basics/04_IIFE.js
@@ -0,0 +1,14 @@
+(function add (num1 , num2){
+    let result = num1+num2
+    console.log(num1+num2)
+})(4,5);
+
+((name)=>{
+    console.log(`hello ${name}`)
+})("pratik")
+
+const addition = (num1,num2) => {
+    return num1+num2
+}
+
+console.log(addition(7,8))
