5d76cf2153fb35bd527f30d580c670517b38a713
Pratik Jagtap <97509037+pratikjagtap4@users.noreply.github.com>
2023-12-28 06:06:17 +0000

    while and do while loops

---
+++ 05_iterators/02_while.js
@@ -0,0 +1,27 @@
+// while loop 
+
+i = 1
+
+while (i<=20) {
+    // console.log(`value of i is ${i}`);
+    i = i+2
+}
+
+const arr = ["pratik", "dhananjay", "swapnil", "tushar","yash","raj","sagar","anurag","prem"]
+
+let index = 0
+
+while(index < arr.length)
+{
+    // console.log(`hi,  my name is ${arr[index]}`);
+    // console.log()
+    index++
+}
+
+// do while loop
+
+let score = 11
+
+do{
+    console.log(`Score is ${score}`);
+} while(score<=10)
---
+++ practise/isstring.js
@@ -0,0 +1,3 @@
+// Q1 Write a JavaScript function to check whether an 'input' is a string or not.
+// Test Data :
+
