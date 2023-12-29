d291a9b02d35eb6e9655717bea68448b54c62f03
Pratik Jagtap <97509037+pratikjagtap4@users.noreply.github.com>
2023-12-29 06:16:06 +0000

    filter for array of objects

---
+++ 05_iterators/06_filter.js
@@ -0,0 +1,51 @@
+const languages = ["c","cpp","javascript","java","python"]
+
+let newlang = languages.forEach((value)=>{
+    return value
+})
+// console.log(newlang);  // for each loop doest not return anything
+
+const arr = [10,20,30,40,50,60,70,80,90,100]
+
+const newarr = arr.filter( (num)=>{
+    return num>40
+} )
+
+// console.log(newarr);
+
+// ***********************************
+const myArr = []
+
+arr.forEach((num) => {
+    if(num>40)
+    {
+        myArr.push(num)
+    }
+})
+
+// console.log(myArr);
+
+// *********************************************/
+
+const books = [
+    { title: 'Book One', genre: 'Fiction', publish: 1981, edition: 2004 },
+    { title: 'Book Two', genre: 'Non-Fiction', publish: 1992, edition: 2008 },
+    { title: 'Book Three', genre: 'History', publish: 1999, edition: 2007 },
+    { title: 'Book Four', genre: 'Non-Fiction', publish: 1989, edition: 2010 },
+    { title: 'Book Five', genre: 'Science', publish: 2009, edition: 2014 },
+    { title: 'Book Six', genre: 'Fiction', publish: 1987, edition: 2010 },
+    { title: 'Book Seven', genre: 'History', publish: 1986, edition: 1996 },
+    { title: 'Book Eight', genre: 'Science', publish: 2011, edition: 2016 },
+    { title: 'Book Nine', genre: 'Non-Fiction', publish: 1981, edition: 1989 },
+];
+
+
+let userBooks = []
+
+userBooks = books.filter( (bk) => {
+    // return (bk.genre === "History")
+    // return (bk.edition >=2010)
+    return ((bk.genre ==="History" || bk.genre === "Science") && bk.publish >= 1995)
+})
+
+console.log(userBooks);
