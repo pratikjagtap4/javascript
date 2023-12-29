8e46dcc4f2df1037dabd087fc1c6691965a8235a
Pratik Jagtap <97509037+pratikjagtap4@users.noreply.github.com>
2023-12-29 06:46:59 +0000

    map and chaining of map and filter

---
+++ 05_iterators/07_map.js
@@ -0,0 +1,33 @@
+const myNums = [1,2,3,4,5,6,7,8,9,10]
+
+const newNums = myNums.map((num)=>
+{
+    return num+10
+})
+
+// console.log(newNums);
+
+
+
+// ********************************** using forEach
+const myNewNums = []
+
+myNums.forEach( (num)=>{
+    let val = num+10
+    myNewNums.push(val)
+} )
+
+// console.log(myNewNums);
+
+
+const values = [1,2,3,4,5,6,7]
+
+let newValues = values.map( (num1,num2)=>{
+    return num1+num2
+})
+
+// console.log(newValues);
+
+const myNewValues = values.map( (num)=>(num*10)).map( (num)=>(num+1)).filter( (num)=>(num>40))
+
+console.log(myNewValues);
