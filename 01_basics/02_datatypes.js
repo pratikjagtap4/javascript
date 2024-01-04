d19f36f8b00d773d2c8a4b7bde582cf57d63c3b9
Pratik Jagtap <97509037+pratikjagtap4@users.noreply.github.com>
2024-01-04 11:49:52 +0000

    datatypes in javascript

--- 01_basics/02_datatypes.js
+++ 01_basics/02_datatypes.js
@@ -4,8 +4,36 @@ let isLoggedIn =true
 let state = null
 let job
 
-// console.log(typeof name);
-// console.log(typeof age);
-// console.log(typeof isLoggedIn);
-console.log(typeof state); // type of null is object 
-console.log(typeof job)
+function sumof(){
+    console.log("hello");
+}
+
+const str = new String("Hello")
+const num = new Number(123)
+const bool = new Boolean("hello")
+const sym = Symbol(123)
+
+// console.log(typeof name)
+// console.log(typeof age)
+// console.log(typeof isLoggedIn)
+// console.log(typeof state) // type of null is object 
+// console.log(typeof job)
+// console.log(typeof sumof)
+// console.log(typeof str)
+// console.log(typeof num)
+// console.log(typeof bool)
+// console.log(typeof sym)
+
+
+const num2 = 123
+
+console.log(Number.parseInt(num2))
+console.log(+{});
+
+const strval = String(sym)
+console.log(strval);
+console.log(typeof strval);
+
+console.log("pratik");
+
+console.log(2/"5");
