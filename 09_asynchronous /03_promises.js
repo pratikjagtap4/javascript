fc65b5f0cd825a9696a71fbf5f7d6e4bc464b01b
Pratik Jagtap <97509037+pratikjagtap4@users.noreply.github.com>
2024-02-08 05:12:24 +0000

    promises

--- 09_asynchronous /03_promises.js
+++ 09_asynchronous /03_promises.js
@@ -2,4 +2,5 @@ const promiseOne = new Promise(function(resolve , reject){
     setTimeout(function(){
         console.log("promise consumed successfully")
     },2000)
-})
+})  
+console.log("Hello")
