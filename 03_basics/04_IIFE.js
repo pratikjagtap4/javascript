6cb6c33e73df0186cdab5519ba36ff79aa018861
Pratik Jagtap <97509037+pratikjagtap4@users.noreply.github.com>
2024-02-09 07:17:13 +0000

    bug because of semicolon

--- 03_basics/04_IIFE.js
+++ 03_basics/04_IIFE.js
@@ -1,21 +1,25 @@
-(function add (num1 , num2){
-    let result = num1+num2
-    console.log(num1+num2)
-})(4,5);
-
-((name)=>{
-    console.log(`hello ${name}`)
-})("pratik")
-
-const addition = (num1,num2) => {
-    return num1+num2
-}
-
-console.log(addition(7,8))
-
-
-(function fun(num1, num2){
+console.log("Before IIFE");
+(function add(num1, num2) {
+    let result = num1 + num2;
     console.log(num1 + num2);
-})(4,5);
+})(4, 5);
 
-fun(10,20)
+console.log("Before arrow function IIFE");
+((name) => {
+    console.log(`hello ${name}`);
+})("pratik");
+
+console.log("Before addition function");
+const addition = (num1, num2) => {
+    return num1 + num2;
+};
+const result = addition(7, 8);
+console.log(result);
+
+
+(function fun(num1, num2) {
+    let result = num1 + num2;
+    console.log(result);
+})(4, 5);
+
+
